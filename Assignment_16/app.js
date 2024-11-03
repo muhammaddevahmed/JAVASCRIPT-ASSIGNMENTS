5484b0fc1024a966ccbc4f692433da6d2dc99780
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2024-11-03 22:25:52 +0500

    Assignment 16

---
+++ Assignment_16/app.js
@@ -0,0 +1,7 @@
+function changeColor(element,color){
+    element.style.backgroundColor = color;
+}
+
+function resetColor(element,originalColor){
+    element.style.backgroundColor = originalColor;
+}
