3441684f0f357fad5f693d010c2bac43b0d3756a
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2024-11-19 12:43:32 +0500

    objects

---
+++ Assignment 21/script1.js
@@ -0,0 +1,189 @@
+var onlineStore = [
+    {
+        name: "TechWorld Online Store",
+        location: "London",
+        category: [
+            "Laptops",
+            "Smartphones",
+            "Accessories",
+        ],
+        products: [
+            {
+
+                id: 1,
+                name: "MackBook Pro",
+                category: "Laptops",
+                Price: "2500$",
+                stock: "200",
+                details: {
+                    brand: "apple",
+                    model: "pro 2024",
+                    colors: ["black", "grey", "white"],
+                    screenSize: "14 inch",
+                },
+            },
+        ],
+        customers: [
+            {
+                id: 1,
+                firstName: "Muhammad",
+                lastName: "Ahmed",
+                email: "m.ahmed.uh72@gmail.com",
+                phoneNumber: "03442681140",
+                addresss: {
+                    street: "st 1 phool galis",
+                    city: "Karachi",
+                    state: "Sindh",
+                    zipCode: "75330",
+
+                },
+
+            },
+        ],
+        orders: [
+            {
+                oId: 1,
+                date: 'createdAt()',
+                status: 0, //0: Shipped , 1: Delivered, 2: Processing,
+                totalAmount: "2500$",
+                products: [
+                    {
+                        productId: 1,
+                        quanity: 5,
+                        Price: "2500$",
+                    }
+                ]
+            }
+        ]
+
+
+
+    },
+    {
+        name: "TechWorld Online Store",
+        location: "London",
+        category: [
+            "Laptops",
+            "Smartphones",
+            "Accessories",
+        ],
+        products: [
+            {
+
+                id: 1,
+                name: "MackBook Pro",
+                category: "Laptops",
+                Price: "2500$",
+                stock: "200",
+                details: {
+                    brand: "apple",
+                    model: "pro 2024",
+                    colors: ["black", "grey", "white"],
+                    screenSize: "14 inch",
+                },
+            },
+        ],
+        customers: [
+            {
+                id: 1,
+                firstName: "Muhammad",
+                lastName: "Ahmed",
+                email: "m.ahmed.uh72@gmail.com",
+                phoneNumber: "03442681140",
+                addresss: {
+                    street: "st 1 phool galis",
+                    city: "Karachi",
+                    state: "Sindh",
+                    zipCode: "75330",
+
+                },
+
+            },
+        ],
+        orders: [
+            {
+                oId: 1,
+                date: 'createdAt()',
+                status: 0, //0: Shipped , 1: Delivered, 2: Processing,
+                totalAmount: "2500$",
+                products: [
+                    {
+                        productId: 1,
+                        quanity: 5,
+                        Price: "2500$",
+                    }
+                ]
+            }
+        ]
+
+
+
+    },
+
+    {
+        name: "TechWorld Online Store",
+        location: "London",
+        category: [
+            "Laptops",
+            "Smartphones",
+            "Accessories",
+        ],
+        products: [
+            {
+
+                id: 1,
+                name: "MackBook Pro",
+                category: "Laptops",
+                Price: "2500$",
+                stock: "200",
+                details: {
+                    brand: "apple",
+                    model: "pro 2024",
+                    colors: ["black", "grey", "white"],
+                    screenSize: "14 inch",
+                },
+            },
+        ],
+        customers: [
+            {
+                id: 1,
+                firstName: "Muhammad",
+                lastName: "Ahmed",
+                email: "m.ahmed.uh72@gmail.com",
+                phoneNumber: "03442681140",
+                addresss: {
+                    street: "st 1 phool galis",
+                    city: "Karachi",
+                    state: "Sindh",
+                    zipCode: "75330",
+
+                },
+
+            },
+        ],
+        orders: [
+            {
+                oId: 1,
+                date: 'createdAt()',
+                status: 0, //0: Shipped , 1: Delivered, 2: Processing,
+                totalAmount: "2500$",
+                products: [
+                    {
+                        productId: 1,
+                        quanity: 5,
+                        Price: "2500$",
+                    }
+                ]
+            }
+        ]
+
+
+
+    },
+]
+
+for (var i = 0; i < onlineStore.length; i++) {
+    var storeDetails = onlineStore[i];
+    console.log(storeDetails.orders);
+   
+}
